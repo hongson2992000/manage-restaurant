a39775ac3de5ef56ef042ea0788ac46525a83d6b
Hong Son <hongson2992000@gmail.com>
2025-02-27 17:06:35 +0700

    setup + code login route handler

---
+++ src/app/api/auth/login/route.ts
@@ -0,0 +1,45 @@
+import authRequest from "@/apiRequests/auth";
+import { LoginBodyType } from "@/schemaValidations/auth.schema";
+import { cookies } from "next/headers";
+import jwt from "jsonwebtoken";
+import { HttpError } from "@/lib/http";
+export async function POST(req: Request) {
+  const res = (await req.json()) as LoginBodyType;
+  const cookieStore = cookies();
+  try {
+    const { payload } = await authRequest.sLogin(res);
+    const { accessToken, refreshToken } = payload.data;
+    const decodeAccessToken = jwt.decode(accessToken) as { exp: number };
+    const decodeRefreshToken = jwt.decode(refreshToken) as { exp: number };
+    (await cookieStore).set("accessToken", accessToken, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "lax",
+      secure: true,
+      expires: decodeAccessToken.exp * 1000,
+    });
+    (await cookieStore).set("refreshToken", refreshToken, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "lax",
+      secure: true,
+      expires: decodeRefreshToken.exp * 1000,
+    });
+    return Response.json(payload);
+  } catch (error) {
+    if (error instanceof HttpError) {
+      return Response.json(error.payload, {
+        status: error.status,
+      });
+    } else {
+      return Response.json(
+        {
+          message: "Internal Server Error",
+        },
+        {
+          status: 500,
+        }
+      );
+    }
+  }
+}
